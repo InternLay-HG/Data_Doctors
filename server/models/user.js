cede2c96367a7df9c83bb651ac5cdc409a4dbd97
GyanPrakash2483 <gyanprakash2483@gmail.com>
2024-11-17 11:22:56 +0530

    Changed isHealthCare datatype from String to Boolean

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,7 @@ const userSchema = mongoose.Schema({
   mobileNumber: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isHealthcareProvider: { type: String, required: true }
+  isHealthcareProvider: { type: Boolean, required: true }
 });
 
 
