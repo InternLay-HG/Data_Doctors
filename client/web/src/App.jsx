ea0238a7f0c34ac15c24312c76b9725b8a123c39
2017kartik <kartikagarwal9119063420@gmail.com>
2024-11-19 09:04:35 +0530

    Fix lazy loading bug

--- client/web/src/App.jsx
+++ client/web/src/App.jsx
@@ -1,49 +1,91 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import HeroLayout from "./components/HeroLayout";
-import PageNotFound from "./components/PageNotFound";
-import CommunitySupport from "./components/CommunitySupport";
 import GlobalStyles from "./style/GlobalStyle";
-import Settings from "./components/Settings";
-import Home from "./components/Home";
+import { Loader1, Loader2 } from "./ui/Loaders";
+import React, { lazy, Suspense } from "react";
+// Lazy-loaded components
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const HeroLayout = lazy(() => import("./components/HeroLayout"));
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
+const Appointment = lazy(() => import("./components/Appointment"));
+const AppointmentScheduling = lazy(() =>
+  import("./components/appointment/Schedule/AppointmentScheduling")
+);
+const UpcomingAppointment = lazy(() =>
+  import("./components/appointment/Upcoming/UpcomingAppointment")
+);
+const PastAppointments = lazy(() =>
+  import("./components/appointment/Past/PastAppointments")
+);
+const NotificationAndReminder = lazy(() =>
+  import("./components/appointment/Notifications/NotificationAndReminder")
+);
+const CommunitySupport = lazy(() => import("./components/CommunitySupport"));
+const Settings = lazy(() => import("./components/Settings"));
+const Home = lazy(() => import("./components/Home"));
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import AppointmentScheduling from "./components/appointment/Schedule/AppointmentScheduling";
-import UpcomingAppointment from "./components/appointment/Upcoming/UpcomingAppointment";
-import PastAppointments from "./components/appointment/Past/PastAppointments";
-import NotificationAndReminder from "./components/appointment/Notifications/NotificationAndReminder";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <GlobalStyles />
-        <Routes>
-          <Route element={<HeroLayout />}>
-            <Route index element={<Navigate replace to="home" />} />
-            <Route path="home" element={<Home />} />
-            <Route path="signup" element={<SignUp />} />
-            <Route path="login" element={<Login />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="appointment" element={<AppointmentScheduling />} />
-            <Route
-              path="appointment/schedule"
-              element={<AppointmentScheduling />}
-            />
-            <Route
-              path="appointment/upcoming"
-              element={<UpcomingAppointment />}
-            />
-            <Route path="appointment/past" element={<PastAppointments />} />
-            <Route
-              path="appointment/notifications"
-              element={<NotificationAndReminder />}
-            />
-            <Route path="community" element={<CommunitySupport />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<Loader1 />}>
+          <Routes>
+            <Route element={<HeroLayout />}>
+              <Route index element={<Navigate replace to="home" />} />
+              <Route
+                path="home"
+                element={
+                  <Suspense fallback={<Loader2 />}>
+                    <Home />
+                  </Suspense>
+                }
+              />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="dashboard"
+                element={
+                  <Suspense fallback={<Loader2 />}>
+                    <Dashboard />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="appointment"
+                element={
+                  <Suspense fallback={<Loader2 />}>
+                    <Appointment />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="appointment/schedule"
+                element={<AppointmentScheduling />}
+              />
+              <Route
+                path="appointment/upcoming"
+                element={<UpcomingAppointment />}
+              />
+              <Route path="appointment/past" element={<PastAppointments />} />
+              <Route
+                path="appointment/notifications"
+                element={<NotificationAndReminder />}
+              />
+              <Route
+                path="community"
+                element={
+                  <Suspense fallback={<Loader2 />}>
+                    <CommunitySupport />
+                  </Suspense>
+                }
+              />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
